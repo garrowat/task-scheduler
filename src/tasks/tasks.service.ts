e352ed2e1b292bd58b3fe7890d03a8d54a4f02e3
Garrett Watson <gwatson@benchsci.com>
2024-07-23 16:31:09 -0600

    Add Tasks Module and Service
    Use Prisma service to create Tasks service.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
-
+import { PrismaService } from 'src/prisma.service';
+import { Task, Prisma } from '@prisma/client';
 @Injectable()
-export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+export class TaskService {
+  constructor(private prisma: PrismaService) {}
+
+  async task(
+    taskWhereUniqueInput: Prisma.TaskWhereUniqueInput,
+  ): Promise<Task | null> {
+    return this.prisma.task.findUnique({
+      where: taskWhereUniqueInput,
+    });
   }
 
-  findAll() {
-    return `This action returns all tasks`;
+  async tasks(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.TaskWhereUniqueInput;
+    where?: Prisma.TaskWhereInput;
+    orderBy?: Prisma.TaskOrderByWithRelationInput;
+  }): Promise<Task[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.task.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async createTask(data: Prisma.TaskCreateInput): Promise<Task> {
+    return this.prisma.task.create({ data });
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async updateTask(params: {
+    where: Prisma.TaskWhereUniqueInput;
+    data: Prisma.TaskUpdateInput;
+  }): Promise<Task> {
+    const { where, data } = params;
+    return this.prisma.task.update({
+      data,
+      where,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async deleteTask(where: Prisma.TaskWhereUniqueInput): Promise<Task> {
+    return this.prisma.task.delete({
+      where,
+    });
   }
 }
