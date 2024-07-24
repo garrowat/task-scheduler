34e99e29f7676d10e7190a4a3fc0e4cfe3a62fd1
Garrett Watson <gwatson@benchsci.com>
2024-07-24 01:09:15 -0600

    Tasks Controller
    Add Tasks controller and routes.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,34 +7,41 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { TaskService } from './tasks.service';
+import { TasksService } from './tasks.service';
+import { Task as TaskModel } from '@prisma/client';
 
-@Controller('tasks')
+@Controller()
 export class TasksController {
-  constructor(private readonly tasksService: TaskService) {}
+  constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
+  @Get('tasks/:id')
+  async getTaskById(@Param('id') id: string): Promise<TaskModel> {
+    return this.tasksService.task({ id: String(id) });
   }
 
-  @Get()
-  findAll() {
-    return this.tasksService.findAll();
+  @Get('tasks')
+  async getAllTasks(): Promise<TaskModel[]> {
+    return this.tasksService.tasks({});
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(+id);
+  @Post('tasks')
+  async createTask(@Body() data: TaskModel): Promise<TaskModel> {
+    return this.tasksService.createTask(data);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
+  @Patch('tasks/:id')
+  async updateTask(
+    @Param('id') id: string,
+    @Body() data: TaskModel,
+  ): Promise<TaskModel> {
+    return this.tasksService.updateTask({
+      where: { id: String(id) },
+      data,
+    });
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+  @Delete('tasks/:id')
+  async deleteTask(@Param('id') id: string): Promise<TaskModel> {
+    return this.tasksService.deleteTask({ id: String(id) });
   }
 }
